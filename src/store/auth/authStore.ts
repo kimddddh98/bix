c9e7e781557b352c832bef141881d4c5f37aa346
kimddddh98 <amawang11@gmail.com>
2026-01-19 02:10:42 +0900

    feat: authStore hasHydrated 상태 추가

--- src/store/auth/authStore.ts
+++ src/store/auth/authStore.ts
@@ -3,6 +3,7 @@ import { persist } from 'zustand/middleware'
 
 interface AuthState {
   accessToken: string | null
+  hasHydrated: boolean
   actions: AuthActions
 }
 
@@ -10,30 +11,34 @@ interface AuthActions {
   login: () => void
   logout: () => Promise<void>
   setAccessToken: (accessToken: string) => void
+  setHasHydrated: (value: boolean) => void
 }
 export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
       accessToken: null,
+      hasHydrated: false,
       user: null,
       actions: {
         login: async () => {},
-
         logout: async () => {
           set({
             accessToken: null,
           })
         },
-
         setAccessToken: async (accessToken: string) => {
           set({ accessToken })
         },
+        setHasHydrated: (value: boolean) => set({ hasHydrated: value }),
       },
     }),
     {
       name: 'auth-storage',
       partialize(state) {
         return { accessToken: state.accessToken }
+      },
+      onRehydrateStorage: () => (state) => {
+        state?.actions.setHasHydrated(true)
       },
     }
   )
