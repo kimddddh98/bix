36d9bb1e42d311af1fdaa88e96a777d0f9df6417
kimddddh98 <amawang11@gmail.com>
2026-01-19 02:15:02 +0900

    test: 401 에러 및 토큰재발급 테스트

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,16 +1,48 @@
 'use client'
+import { logout } from '@/api/auth/auth'
+import http from '@/api/axios'
+import useAuth from '@/hooks/auth/useAuth'
 import { useAuthStore } from '@/store/auth/authStore'
-import React from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter()
+  const { getRotateToken } = useAuth()
   const accessToken = useAuthStore((state) => state.accessToken)
+  const hasHydrated = useAuthStore((state) => state.hasHydrated)
+
+  const checkToken = async () => {
+    try {
+      await getRotateToken()
+    } catch (e) {
+      console.log(e)
+      logout()
+      router.replace('/signin')
+    }
+  }
+  const test = async () => {
+    const res = await http.post('/api/test')
+  }
+
+  useEffect(() => {
+    if (!hasHydrated) return
+    if (!accessToken) {
+      checkToken()
+    }
+  }, [accessToken, hasHydrated])
+
   return (
     <div>
       여기는 레이아웃 {accessToken}
+      <button className="border" onClick={test}>
+        재발급 버튼
+      </button>
       {children}
     </div>
   )
