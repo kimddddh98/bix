aa6f495b0473be354167b014cc32b7ae6ab5bb68
kimddddh98 <amawang11@gmail.com>
2026-01-19 02:54:33 +0900

    feat: 라우트 핸들러 에러 처리

--- src/app/api/auth/refresh/route.ts
+++ src/app/api/auth/refresh/route.ts
@@ -3,26 +3,32 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
-  const cookieStore = await cookies() //
-  const refreshToken = cookieStore.get('refreshToken')
+  try {
+    const cookieStore = await cookies() //
+    const refreshToken = cookieStore.get('refreshToken')
 
+    if (refreshToken?.value) {
+      const data = await rotateToken(refreshToken.value)
 
-  if (refreshToken?.value) {
-    const data = await rotateToken(refreshToken.value)
+      const cookieStore = await cookies()
+      cookieStore.set('refreshToken', data.refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
+      })
 
-    const cookieStore = await cookies()
-    cookieStore.set('refreshToken', data.refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      path: '/',
-    })
-
-    return NextResponse.json(data)
+      return NextResponse.json(data)
+    }
+  } catch (error: any) {
+    if (error.response) {
+      return NextResponse.json(error.response.data, {
+        status: error.response.status,
+      })
+    }
   }
-
   return NextResponse.json(
-    { message: '로그인에 실패했습니다.' },
+    { message: '로그인이 만료되었습니다.' },
     { status: 401 }
   )
 }
--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -3,24 +3,26 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
-  const body = (await request.json()) as SignInRequestParams
+  try {
+    const body = (await request.json()) as SignInRequestParams
 
-  const data = await signIn(body)
+    const data = await signIn(body)
 
-  if (data.refreshToken) {
-    const cookieStore = await cookies()
-    cookieStore.set('refreshToken', data.refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      path: '/',
-    })
-
+    if (data.refreshToken) {
+      const cookieStore = await cookies()
+      cookieStore.set('refreshToken', data.refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
+      })
+    }
     return NextResponse.json(data)
+  } catch (error: any) {
+    if (error.response) {
+      return NextResponse.json(error.response.data, {
+        status: error.response.status,
+      })
+    }
   }
-
-  return NextResponse.json(
-    { message: '로그인에 실패했습니다.' },
-    { status: 401 }
-  )
 }
