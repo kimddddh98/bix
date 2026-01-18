7254298a70183cb8e806e362ffdcb8ae5756774f
kimddddh98 <amawang11@gmail.com>
2026-01-19 02:11:14 +0900

    feat: NEXT_PUBLIC_API_ROUTE_URL 환경변수 추가

--- src/const/urls.const.ts
+++ src/const/urls.const.ts
@@ -1,6 +1,8 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+const API_ROUTE_URL = process.env.NEXT_PUBLIC_API_ROUTE_URL
 export const URL = {
   baseUrl: API_BASE_URL,
+  apiRouteUrl: API_ROUTE_URL,
 } as const
 
 export default URL
