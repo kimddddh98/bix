6ef023cc32a5cf6b1e60904c0e109b76a31b5007
kimddddh98 <amawang11@gmail.com>
2026-01-19 16:57:10 +0900

    ui: 작성 모달 컨텐츠 컴포넌트로 분리

--- src/app/@modal/(protected)/(.)write/page.tsx
+++ src/app/@modal/(protected)/(.)write/page.tsx
@@ -1,46 +1,20 @@
+'use client'
+
+import WriteModal from '@/components/posts/WriteModal'
+import { useRouter } from 'next/navigation'
+
 export default function ModalWritePage() {
+  const router = useRouter()
+  const closeModal = () => {
+    router.back()
+  }
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
+    <div
+      onClick={closeModal}
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/40"
+    >
       {/* Modal Container */}
-      <div className="w-full max-w-md rounded-2xl bg-white shadow-xl">
-        {/* Header */}
-        <header className="flex items-center justify-between border-b px-4 py-3">
-          <button type="button" className="text-sm text-gray-500">
-            취소
-          </button>
-          <h2 className="text-base font-semibold text-gray-900">글 작성</h2>
-          <button type="button" className="text-sm font-medium text-black">
-            등록
-          </button>
-        </header>
-
-        {/* Content */}
-        <div className="space-y-4 px-4 py-4">
-          {/* Title */}
-          <div className="space-y-1">
-            <label className="block text-sm font-medium text-gray-700">
-              제목
-            </label>
-            <input
-              type="text"
-              placeholder="제목을 입력하세요"
-              className="w-full rounded-xl border px-3 py-2 text-sm focus:ring-1 focus:ring-black focus:outline-none"
-            />
-          </div>
-
-          {/* Body */}
-          <div className="space-y-1">
-            <label className="block text-sm font-medium text-gray-700">
-              내용
-            </label>
-            <textarea
-              rows={6}
-              placeholder="내용을 입력하세요"
-              className="w-full resize-none rounded-xl border px-3 py-2 text-sm focus:ring-1 focus:ring-black focus:outline-none"
-            />
-          </div>
-        </div>
-      </div>
+      <WriteModal />
     </div>
   )
 }
---
+++ src/components/posts/WriteModal.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { WritePostRequsetParams } from '@/api/posts/posts'
+import useCategoriesQuery from '@/hooks/queries/useCategoriesQuery'
+import { useForm } from 'react-hook-form'
+import CategorySelectBox from './CategorySelectBox'
+
+const WriteModal = () => {
+  const {} = useForm<WritePostRequsetParams>()
+
+  return (
+    <div
+      className="w-full max-w-md rounded-2xl bg-white shadow-xl"
+      onClick={(e) => e.stopPropagation()}
+    >
+      {/* Header */}
+      <header className="flex items-center justify-between border-b border-b-gray-400 px-4 py-3">
+        <button type="button" className="text-sm text-gray-500">
+          취소
+        </button>
+        <h2 className="text-base font-semibold text-gray-900">글 작성</h2>
+        <button type="button" className="text-sm font-medium text-black">
+          등록
+        </button>
+      </header>
+
+      <div className="flex flex-col gap-4 px-4 py-4">
+        {/* Title */}
+        <div className="space-y-1">
+          <label className="block text-sm font-medium text-gray-700">
+            제목
+          </label>
+          <input
+            type="text"
+            placeholder="제목을 입력하세요"
+            className="w-full rounded-xl border px-3 py-2 text-sm focus:ring-1 focus:ring-black focus:outline-none"
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label className="block text-sm font-medium text-gray-700">
+            카테고리
+          </label>
+          <CategorySelectBox />
+        </div>
+
+        {/* Body */}
+        <div className="space-y-1">
+          <label className="block text-sm font-medium text-gray-700">
+            내용
+          </label>
+          <textarea
+            rows={6}
+            placeholder="내용을 입력하세요"
+            className="w-full resize-none rounded-xl border px-3 py-2 text-sm focus:ring-1 focus:ring-black focus:outline-none"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WriteModal
