1d06e75cb6f1f9f399bb7e50eeba6a968162bd0c
kimddddh98 <amawang11@gmail.com>
2026-01-19 18:40:43 +0900

    feat: 글목록 조회중 스켈레톤 ui 랜더링

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -4,27 +4,30 @@ import PostListItem from './PostListItem'
 import useCategoriesQuery from '@/hooks/queries/useCategoriesQuery'
 import usePostListQuery from '@/hooks/queries/usePostListQuery'
 import Link from 'next/link'
+import PostListSkeleton from './PostListSkeleton'
 
 const PostList = () => {
   const { data: categories } = useCategoriesQuery()
-  const { data } = usePostListQuery()
+  const { data, isPending } = usePostListQuery()
   const posts = data?.content
   return (
     <div>
-      {posts && (
+      {isPending ? (
+        <PostListSkeleton />
+      ) : posts && posts?.length > 0 ? (
         <ul className="flex flex-col gap-4">
           {categories &&
             posts.map((post) => (
               <PostListItem key={post.id} post={post} categories={categories} />
             ))}
         </ul>
+      ) : (
+        <div className="col-span-full flex min-h-75 items-center justify-center">
+          <h3 className="text-lg font-medium text-gray-600">
+            게시글이 존재하지 않습니다.
+          </h3>
+        </div>
       )}
-
-      <div className="col-span-full flex min-h-75 items-center justify-center">
-        <h3 className="text-lg font-medium text-gray-600">
-          게시글이 존재하지 않습니다.
-        </h3>
-      </div>
 
       <Link
         href={'/write'}
